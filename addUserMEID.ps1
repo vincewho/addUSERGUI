1bdc4e77c13200f6fcfdb44057a493c468a7aa2e
Vincent Li <vrli1@asu.edu>
2019-08-18 15:42:29 -0700

    initial .ps1 file

---
+++ addUserMEID.ps1
@@ -0,0 +1,39 @@
+﻿<# This form was created using POSHGUI.com  a free online gui designer for PowerShell
+.NAME
+    Untitled
+#>
+
+Add-Type -AssemblyName System.Windows.Forms
+[System.Windows.Forms.Application]::EnableVisualStyles()
+
+$Form                            = New-Object system.Windows.Forms.Form
+$Form.ClientSize                 = '355,125'
+$Form.text                       = "Form"
+$Form.TopMost                    = $false
+
+$meid_lbl                        = New-Object system.Windows.Forms.Label
+$meid_lbl.text                   = "Enter MEID:"
+$meid_lbl.AutoSize               = $true
+$meid_lbl.width                  = 25
+$meid_lbl.height                 = 10
+$meid_lbl.location               = New-Object System.Drawing.Point(47,28)
+$meid_lbl.Font                   = 'Microsoft Sans Serif,10'
+
+$add_btn                         = New-Object system.Windows.Forms.Button
+$add_btn.text                    = "Add"
+$add_btn.width                   = 60
+$add_btn.height                  = 30
+$add_btn.location                = New-Object System.Drawing.Point(135,64)
+$add_btn.Font                    = 'Microsoft Sans Serif,10'
+
+$meid_txtbox                     = New-Object system.Windows.Forms.TextBox
+$meid_txtbox.multiline           = $false
+$meid_txtbox.width               = 163
+$meid_txtbox.height              = 20
+$meid_txtbox.location            = New-Object System.Drawing.Point(135,24)
+$meid_txtbox.Font                = 'Microsoft Sans Serif,10'
+
+$Form.controls.AddRange(@($meid_lbl,$add_btn,$meid_txtbox))
+
+
+[void]$Form.ShowDialog()
