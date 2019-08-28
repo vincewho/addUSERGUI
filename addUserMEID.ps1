4e409cb4311b3a8a71dc32ba73141440da02b6d6
Li <VIN2164329@maricopa.edu>
2019-08-28 08:41:20 -0700

    added check button and logic for check

--- addUserMEID.ps1
+++ addUserMEID.ps1
@@ -24,57 +24,42 @@ function Start-Job{
 		Start-DbaAgentJob -SqlInstance $sqlserver -Job $job
 }
 
+function Find-Person {
+	Param (
+		[Parameter(Mandatory=$true)] [string] $usermeid
+		)
+	$sqlserver = 'it14'
+	$db = 'idCard'
+	$query = "Select * from dbo.EPI_PERSON where MEID = '$usermeid'"
+	$exist = $null
+	Invoke-DbaQuery -SqlInstance $sqlserver -Database $db -Query $query
+}
+
 $on_click = { 
     # Get-OnePerson $meid_txtbox.Text
     # Start-Job
     $Form.Close()
 }
 
-##############################
-# Creat the form
-##############################
-
-
-<<<<<<< HEAD
-function Get-OnePerson{
-	Param (
-		[Parameter(Mandatory=$true)] [string] $usermeid
-		)
-    # $output = $PSScriptRoot + '\output.csv'
-    $output = '\\it14\scripts\id_card\output.csv'
-	Get-ADUser -filter * -Server "mcccd.org"  -Properties EmployeeID,Created,whenChanged,Surname,GivenName,middleName,mccdLogonID,mccdEmployeeTypeDesc,mccdStudentID,Title,CanonicalName -SearchBase "OU=Employees,OU=MARICOPA,DC=mcccd,DC=org" | 
-	Select-Object EmployeeID,Created,whenChanged,Surname,GivenName,middleName,mccdLogonID,mccdEmployeeTypeDesc,mccdStudentID,Title,CanonicalName | 
-	Where-Object { $_.mccdLogonID -eq $usermeid } | 
-	ConvertTo-Csv -NoTypeInformation | 
-	% {$_.Replace('"','')} | 
-	Out-File $output -Force
-}
-
-function Start-Job{
-		$sqlserver = 'it14'
-		$job = 'NewIDcardFeed'
-		Start-DbaAgentJob -SqlInstance $sqlserver -Job $job
-}
-
-$on_click = { 
-    Get-OnePerson $meid_txtbox.Text
-    Start-Job
-    $Form.Close()
+$chk_click = {
+    $person = $null
+    $person = Find-Person -usermeid $meid_txtbox.Text
+    if ($person -ne $null ){
+        [System.Windows.Forms.MessageBox]::Show("User is found" , "Find User")
+    } else{
+        [System.Windows.Forms.MessageBox]::Show("User cannot be found" , "Find User")
+    }
 }
 
 ##############################
 # Creat the form
 ##############################
 
-Add-Type -AssemblyName System.Windows.Forms
-[System.Windows.Forms.Application]::EnableVisualStyles()
-
-=======
->>>>>>> 07e487e8545a9ec0b4eb1293edf05da3c5a61a67
 $Form                            = New-Object system.Windows.Forms.Form
 $Form.ClientSize                 = '413,119'
 $Form.text                       = "AddForm"
-$Form.TopMost                    = $false
+$Form.TopMost                    = $True
+$Form.StartPosition              = "CenterScreen"
 
 $meid_txtbox                     = New-Object system.Windows.Forms.TextBox
 $meid_txtbox.multiline           = $false
@@ -87,8 +72,16 @@ $add_btn                         = New-Object system.Windows.Forms.Button
 $add_btn.text                    = "Add"
 $add_btn.width                   = 60
 $add_btn.height                  = 30
-$add_btn.location                = New-Object System.Drawing.Point(186,80)
+$add_btn.location                = New-Object System.Drawing.Point(200,80)
 $add_btn.Font                    = 'Microsoft Sans Serif,10'
+
+$check_btn                         = New-Object system.Windows.Forms.Button
+$check_btn.text                    = "Check"
+$check_btn.width                   = 60
+$check_btn.height                  = 30
+$check_btn.location                = New-Object System.Drawing.Point(100,80)
+$check_btn.Font                    = 'Microsoft Sans Serif,10'
+
 
 $meid_lbl                        = New-Object system.Windows.Forms.Label
 $meid_lbl.text                   = "MEID"
@@ -97,23 +90,6 @@ $meid_lbl.width                  = 25
 $meid_lbl.height                 = 10
 $meid_lbl.location               = New-Object System.Drawing.Point(88,30)
 $meid_lbl.Font                   = 'Microsoft Sans Serif,10'
-<<<<<<< HEAD
-
-$notice_lbl                      = New-Object system.Windows.Forms.Label
-$notice_lbl.text                 = "There will be a delay of around 1 minute before this box closes"
-$notice_lbl.AutoSize             = $true
-$notice_lbl.width                = 25
-$notice_lbl.height               = 10
-$notice_lbl.location             = New-Object System.Drawing.Point(14,56)
-$notice_lbl.Font                 = 'Microsoft Sans Serif,10'
-
-$Form.controls.AddRange(@($meid_txtbox,$add_btn,$meid_lbl,$notice_lbl))
-
-##############################
-# When Button is pressed
-##############################
-=======
->>>>>>> 07e487e8545a9ec0b4eb1293edf05da3c5a61a67
 
 $notice_lbl                      = New-Object system.Windows.Forms.Label
 $notice_lbl.text                 = "There will be a delay of around 20 seconds before this box closes"
@@ -123,16 +99,13 @@ $notice_lbl.height               = 10
 $notice_lbl.location             = New-Object System.Drawing.Point(14,56)
 $notice_lbl.Font                 = 'Microsoft Sans Serif,10'
 
-<<<<<<< HEAD
-=======
-$Form.controls.AddRange(@($meid_txtbox,$add_btn,$meid_lbl,$notice_lbl))
-
 ##############################
 # When Button is pressed
 ##############################
 
 $add_btn.Add_Click($on_click)
+$check_btn.Add_Click($chk_click)
 
->>>>>>> 07e487e8545a9ec0b4eb1293edf05da3c5a61a67
+$Form.controls.AddRange(@($meid_txtbox,$add_btn, $check_btn,$meid_lbl,$notice_lbl))
 
 [void]$Form.ShowDialog()
