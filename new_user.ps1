1881916991632cb62b861b31a67ed581ef381a5b
vincewho <vincewho@MacBook-Pro.local>
2020-02-19 21:46:26 -0800

    added new ps1

--- new_user.ps1
+++ new_user.ps1
@@ -24,20 +24,6 @@ function create_sDrive{
 
 }
 
-function move_appdata{
-    # $machine = Read-Host -Prompt 'Input the old machine name'
-    # $new_machine = Read-Host -Prompt 'Input the new machine name'
-    # $username = Read-Host -Prompt 'Input the user name'
-
-    # write-host $username $machine
-
-    # Get-ChildItem \\$machine\c$\Users\$username\AppData\Local\Google
-    # Get-ChildItem \\$new_machine\c$\Users\$username\AppData\Local\Google
-
-    # Get-ChildItem $env:APPDATA\Adobe\Acrobat\2015\Security
-
-}
-
 function Show-Menu {
     param (
         [string]$title = 'Menu'
