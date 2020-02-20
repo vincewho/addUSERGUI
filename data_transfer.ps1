4b50cc604e6a0fae8328f34a00dedb100646b1a1
vincewho <vincewho@MacBook-Pro.local>
2020-02-19 21:31:55 -0800

    master

--- data_transfer.ps1
+++ data_transfer.ps1
@@ -1,22 +1,10 @@
-# $machine = 'P-WS049'
-
-# $machine = Read-Host -Prompt 'Input the old machine name'
-# $new_machine = Read-Host -Prompt 'Input the new machine name'
-# $username = Read-Host -Prompt 'Input the user name'
-
-# write-host $username $machine
-
-# Get-ChildItem \\$machine\c$\Users\$username\AppData\Local\Google
-# Get-ChildItem \\$new_machine\c$\Users\$username\AppData\Local\Google
-
-# Get-ChildItem $env:APPDATA\Adobe\Acrobat\2015\Security
-
 function get_new_user{
-    $user_fname = Read-Host "What is the user First Name?"
-    $user_lname = Read-Host "What is the user Last name?"
-    $dept = Read-Host "Are they KROST or KBKG?"
+    $user_fname = Read-Host 'What is the user First Name?'
+    $user_lname = Read-Host 'What is the user Last name?'
+    $dept = Read-Host 'Are they KROST or KBKG?'
     return $user_fname, $user_lname, $dept
 }
+
 function copy_user{
         $new_user = get_new_user
         $temp_email = $new_user[0]+"."+$new_user[1]
@@ -32,10 +20,21 @@ function copy_user{
         return $official_email
 }
 
-        # $template_user = Get-ADUser test.testingtonson
-        # Write-Host $template_user.Name "is input"
-
 function create_sDrive{
+
+}
+
+function move_appdata{
+    # $machine = Read-Host -Prompt 'Input the old machine name'
+    # $new_machine = Read-Host -Prompt 'Input the new machine name'
+    # $username = Read-Host -Prompt 'Input the user name'
+
+    # write-host $username $machine
+
+    # Get-ChildItem \\$machine\c$\Users\$username\AppData\Local\Google
+    # Get-ChildItem \\$new_machine\c$\Users\$username\AppData\Local\Google
+
+    # Get-ChildItem $env:APPDATA\Adobe\Acrobat\2015\Security
 
 }
 
@@ -47,10 +46,9 @@ function Show-Menu {
     Write-Host "================ $Title ================"
 
     Write-Host "1: Press '1' to add user to AD."
-    Write-Host "2: Press '2' for this option."
+    Write-Host "2: Press '2' Copy app data"
     Write-Host "3: Press '3' for this option."
     Write-Host "Q: Press 'Q' to quit."
-    
 }
 
 do{ 
@@ -58,6 +56,8 @@ do{
    $selected = Read-Host "Please make a selection"
    switch ($selected) {
        '1' { copy_user }
+       '2' { move_appdata }
+       '3' { createI_sDrive }
        Default { $selected = 'q' }
    }
    Pause
